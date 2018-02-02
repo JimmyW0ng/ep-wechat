694865fa716218ea7bdfc67aefbb2c190957b081
misCC <1518265554@qq.com>
2018-02-02 15:12:22 +0800

    Update

--- pages/orgnization/OrgnizationPage.js
+++ pages/orgnization/OrgnizationPage.js
@@ -2,49 +2,66 @@
 const AXIOS = require('../../utils/axios')
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    title: '精选培训机构',
-
-    orgnizationList: [{
-      orgName: '金宝贝早教中心文三路店',
-      orgAddress: '西湖区文三西路221号',
-      avatar: 'http://res.xiaomaiketang.com/xiaomai/app-registration_201801015.png',
-      point: 'TODO',
-      tagList: ['很好', '很强大', '很暴力'],
-      learnNum: 12192
-    }, {
-      orgName: '钓的一笔教育机构',
-      orgAddress: '上海海上海浙江教育路23号',
-      avatar: 'http://res.xiaomaiketang.com/xiaomai/app-newYearDay_201701208.png',
-      point: 'TODO',
-      tagList: ['很好', '很强大', '很暴力', '很暴力', '很暴力', '很暴力', '很暴力'],
-      learnNum: 1219
-      }, {
-        orgName: '非常好的机构',
-        orgAddress: '西湖区文三西路221号',
-        avatar: 'http://res.xiaomaiketang.com/xiaomai/app-halloween_2017010016.png',
-        point: 'TODO',
-        tagList: ['很好'],
-        learnNum: 12
-    }, {
-      orgName: '绍兴机构和哈哈教育中心',
-      orgAddress: '绍兴市越城区小管路22号',
-      avatar: 'http://res.xiaomaiketang.com/xiaomai/labourDay_201704021.png',
-      point: 'TODO',
-      tagList: ['很好', '很强大', '很暴力'],
-      learnNum: 12192
-    }]
+    resPupet: {
+      "error": "string",
+      "errorDescription": "string",
+      "result": {
+        "content": [{
+          "catalogIds": "string",
+          "catalogLabelFormat": [
+            "string"
+          ],
+          "fileUrl": "string",
+          "id": 0,
+          "marketWeight": "string",
+          "organAddress": "string",
+          "organCreateDate": {
+            "date": 0,
+            "day": 0,
+            "hours": 0,
+            "minutes": 0,
+            "month": 0,
+            "nanos": 0,
+            "seconds": 0,
+            "time": 0,
+            "timezoneOffset": 0,
+            "year": 0
+          },
+          "organEmail": "string",
+          "organIntroduce": "string",
+          "organLat": "string",
+          "organLng": "string",
+          "organName": "string",
+          "organPhone": "string",
+          "organRegion": 0,
+          "organShortIntroduce": "string",
+          "organUrl": "string",
+          "status": "normal",
+          "togetherScore": "string",
+          "totalParticipate": 0
+        }],
+        "first": true,
+        "last": true,
+        "number": 0,
+        "numberOfElements": 0,
+        "size": 0,
+        "sort": {},
+        "totalElements": 0,
+        "totalPages": 0
+      },
+      "success": true
+    },
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -53,15 +70,16 @@ Page({
   onReady: function () {
     const self = this
     AXIOS.POST('security/organ/page', {
-      page: 1
+      page: 0
     }, (res) => {
       const result = res.result || {}
       const content = result.content || []
-      var param = {}
-      var string = "orgnizationList[" + 0 + "].point";
-      param[string] = content[0].organShortIntroduce;
-      console.log('param',param)
-      self.setData(param)
+      // var param = {}
+      // var string = "orgnizationList[" + 0 + "].point";
+      // param[string] = content[0].organShortIntroduce;
+      // console.log('param',param)
+      console.log('content')
+      self.setData({ orgnizationList: content })
     })
   },
 
@@ -69,41 +87,41 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
