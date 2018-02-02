425edfa19a728ea71840cee0e03f16352130a8e8
ilovezy <1518265554@qq.com>
2018-02-03 00:13:09 +0800

    Update

--- pages/orgnization/OrgnizationPage.js
+++ pages/orgnization/OrgnizationPage.js
@@ -11,6 +11,12 @@ Page({
     last: false
   },
 
+  handleGoDetail() {
+    wx.navigateTo({
+      url: './orgnizationDetail/OrganizationDetailPage'
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
