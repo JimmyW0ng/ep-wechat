e17e3f0e4248c575c75ebc46aa7314c27ee3580e
misCC <1518265554@qq.com>
2018-02-03 18:27:39 +0800

    update

--- pages/orgnization/orgnizationDetail/OrganizationDetailPage.js
+++ pages/orgnization/orgnizationDetail/OrganizationDetailPage.js
@@ -9,7 +9,16 @@ Page({
   data: {
     cover: '../../../asset/img/org-cover.jpg',
     ognBanners: [],
-    ognInfo: {}
+    ognInfo: {},
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
 
   /**
