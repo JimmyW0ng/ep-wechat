8751382e67fc04ba3b42384dd6428c3296c2814c
ilovezy <1518265554@qq.com>
2018-02-04 11:40:41 +0800

    update

--- pages/orgnization/orgnizationDetail/OrganizationDetailPage.js
+++ pages/orgnization/orgnizationDetail/OrganizationDetailPage.js
@@ -1,5 +1,6 @@
 // pages/orgnization/orgnizationDetail/OrganizationDetailPage.js
 const AXIOS = require('../../../utils/axios')
+const WxParse = require('../../../utils/wxParse/wxParse.js');
 
 Page({
 
@@ -10,27 +11,23 @@ Page({
     cover: '../../../asset/img/org-cover.jpg',
     ognBanners: [],
     ognInfo: {},
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: false,
+    ognCourseList: [],
+    indicatorDots: true,
     autoplay: false,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    test: '<p>hello wrold</p>'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
-
-    this.getData(options.id)
+    this.getOgnData(options.id)
+    this.getOgnCourseData(options.id)
   },
 
-  getData(id) {
+  getOgnData(id) {
     const self = this
     AXIOS.POST('security/organ/detail', {
       organId: id
@@ -43,52 +40,64 @@ Page({
     })
   },
 
+  getOgnCourseData(id) {
+    const self = this
+    AXIOS.POST('security/course/page', {
+      organId: id
+    }, (res) => {
+      const result = res.result || {}
+      self.setData({
+        ognCourseList: result.content || [],
+        last: result.last,
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
