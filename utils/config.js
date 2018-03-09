8d8b0ebcb645f9221b628e8066df36b88c882fb2
misCC <1518265554@qq.com>
2018-03-09 15:17:26 +0800

    Change api url

--- utils/config.js
+++ utils/config.js
@@ -3,10 +3,8 @@ let token = User.getToken()
 let memberType = User.getMemberType()
 
 const CONFIG = {
-  // apiDocUrl: 'http://122.225.218.26:9009/swagger-ui.html',
-  apiDocUrl: 'http://ep2.viphk1.ngrok.org/swagger-ui.html',
-  // apiUrl: 'http://122.225.218.26:9009/',
-  apiUrl: 'http://ep2.viphk1.ngrok.org/',
+  apiDocUrl: 'http://ep3.viphk.ngrok.org/swagger-ui.html',
+  apiUrl: 'http://ep3.viphk.ngrok.org/',
   
   mySqlUrl: 'jdbc:mysql://122.225.218.26:9002/ep?useUnicode=true&characterEncoding=utf-8&useSSL=false',
 
